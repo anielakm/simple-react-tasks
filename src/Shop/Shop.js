7dee8d4d869ab026a31b80efcbe891de34095261
Aniela <kontakt@anielakm.pl>
2019-09-17 16:35:25 +0200

    moved form to individual component

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -34,18 +34,7 @@ class Shop extends Component {
         return (<>
 
             <h1>Twój koszyk:</h1>
-
             <Form ammountState={this.state} handleButton={this.handleButton} />
-
-            {/* <button disabled={this.state.ammount === 0 ? true : false} id="minus" onClick={this.handleButton}>-</button>
-            {this.state.ammount}/{this.state.maxAmmount}
-            <button disabled={this.state.ammount === this.state.maxAmmount ? true : false} id="plus" onClick={this.handleButton}>+</button>
-            <br />
-            <button id="buy" disabled={this.state.maxAmmount === 0 ? true : false} onClick={this.handleButton}>Kup</button><br />
-            Zakupiono: {this.state.totalSoldAmmount} <br />
-            Ilość dostępnych sztuk: {this.state.maxAmmount} */}
-
-
 
         </>
         );
